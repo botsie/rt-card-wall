a443e847f71531c8670518f61d2b5d9f3d812782
Biju Chacko <biju.chacko@gmail.com>
2012-08-31 15:58:51 +0530

    Initial modularization code

--- application.rb
+++ application.rb
@@ -2,17 +2,20 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 require 'pp'
-require 'ruby-prof'
+
 require File.expand_path('../rt.rb', __FILE__)
 
+get '/new-q/:queue' do
+  card_wall_controller = CardWallController.new
+  card_wall_controller.show(params)
+end
+
 get '/q/:queue' do
-  # RubyProf.start
   rt = RT::Server.new
   tickets = RT::Query.new(rt)
   @queue = params[:queue]
-  # pp queue
+
   results = tickets.where('Queue').is(@queue).execute
-  # pp results
 
   @cards = Hash.new
   # @statuses = [ 'new', 'open', 'stalled', 'resolved', 'rejected', 'deleted' ]
@@ -22,13 +25,39 @@ get '/q/:queue' do
     @cards[status] = results.select { |t| t['Status'] == status }
   end
 
-  # pp @cards
-  
-  # result = RubyProf.stop
-
-  # printer = RubyProf::GraphHtmlPrinter.new(result)
-  # File.open('profile.log', 'w') do |f|
-  #   printer.print(f)
-  # end
   haml :card_wall
+end
+
+class CardWallController
+  def show(params)
+
+    rt = RT::Server.new
+    tickets = RT::Query.new(rt)
+    @queue = params[:queue]
+
+    results = tickets.where('Queue').is(@queue).execute
+
+
+    @cards = Hash.new
+    @statuses = [ 'new', 'open', 'stalled', 'resolved' ]
+
+    @statuses.each do |status|
+      @cards[status] = results.select { |t| t['Status'] == status }
+    end
+
+    haml :card_wall
+  end
+
+  def haml( template_id )
+    layout = File.read('views/layout.haml')
+    template = File.read('views/' + template_id.to_s + '.haml')
+    layout_engine = Haml::Engine.new(layout)
+    layout_engine.render(self) do
+      template_engine = Haml::Engine.new(template)
+      template_engine.render(self)
+    end
+  end
+end
+
+class CardWallModel
 end
